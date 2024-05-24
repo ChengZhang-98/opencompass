ef7b14a1e10a27b1829fc183f3a90613bd19aeb7
Cheng Zhang <chengzhang98@outlook.com>
2024-05-24 20:07:47 +0100

    add custom model

---
+++ opencompass/models/huggingface_ruruquant.py
@@ -0,0 +1,52 @@
+from .huggingface_above_v4_33 import HuggingFaceBaseModel
+
+
+class HuggingFaceBaseModelRuruQuant(HuggingFaceBaseModel):
+    def __init__(
+        self,
+        path: str,
+        q_config: dict,
+        model_kwargs: dict = dict(),
+        tokenizer_path: str | None = None,
+        tokenizer_kwargs: dict = dict(),
+        peft_path: str | None = None,
+        peft_kwargs: dict = dict(),
+        tokenizer_only: bool = False,
+        generation_kwargs: dict = dict(),
+        max_seq_len: int | None = None,
+        pad_token_id: int | None = None,
+        stop_words: str | None = [],
+        **other_kwargs
+    ):
+        super().__init__(
+            path,
+            model_kwargs,
+            tokenizer_path,
+            tokenizer_kwargs,
+            peft_path,
+            peft_kwargs,
+            tokenizer_only,
+            generation_kwargs,
+            max_seq_len,
+            pad_token_id,
+            stop_words,
+            **other_kwargs
+        )
+
+        self.q_config = q_config
+
+    def _load_model(
+        self,
+        path: str,
+        kwargs: dict,
+        peft_path: str | None = None,
+        peft_kwargs: dict = dict(),
+    ):
+        from ruruquant.models.automodel import AutoRuruQuantModelForCausalLM
+
+        self.model = AutoRuruQuantModelForCausalLM.from_pretrained(
+            path, self.q_config, **kwargs
+        )
+
+        self.model.eval()
+        self.model.generation_config.do_sample = False
